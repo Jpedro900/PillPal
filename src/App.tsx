ea425b5002081d466f8a6edcee777f5f6a237b7c
Jpedro900 <jpedro.cs09@gmail.com>
2024-05-01 20:57:34 -0300

    First page complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <img id='logo-pill-pal' src="src\assets\LogoCompleto.svg" alt="LogoPillPal"/>
+      <div className="texts">
+        <h1>Login</h1>
+        <p>Use seu e-mail para se conectar</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form action="" method="post" className='form'>
+        <div className="input-field">
+            <input type="email" name="femail" id="femail" required/>
+            <div className="labelline">E-mail</div>
+        </div>
+        <div className="input-field">
+          <input type="password" name="fpassword" id="fpassword" required/>
+          <div className="labelline">Senha</div>
+        </div>
+          <button type="submit">Entrar</button>
+        
+      </form>
+      <div className="options">
+        <a href="#">Esqueci a senha?</a>
+        <a href="#">Não tem uma conta? Cadastre-se</a>
+      </div>
     </>
   )
 }
